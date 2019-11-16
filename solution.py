58a1b5608bbd7531acf53002adfcad6cab4e30b4
Madina <madina9997@mail.ru>
2019-11-17 00:17:42 +0300

    Add files via upload

--- solution.py
+++ solution.py
@@ -4,12 +4,14 @@ import pandas as pd
 import numpy as np
 
 def main():
-    # print command line arguments
     input_dir, output_dir = sys.argv[1:]
-    predicted_result = []
+    # opening of initial data and converting it to dataframe structure
+    # of numpy package and then into pandas in order to have names for columns
     df = np.loadtxt(input_dir + '/data.data')
     df = pd.DataFrame(df, columns=['column 1', 'column 2'])
+    # creating new column where stored the sum of two previous columns
     df['result'] = df['column 1'] + df['column 2']
+    # saving to file
     np.savetxt(output_dir + '/data.predict', np.array(df['result']))
     return 0
 
